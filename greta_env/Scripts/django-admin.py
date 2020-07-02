30020896ec9d705d57167356dbc49d60f2c22026
unknown <joemar.greta16@gmail.com>
2020-07-02 20:07:48 +0800

    Make IT Better - Greta

---
+++ greta_env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\greta\desktop\python\projects\persocialite\greta_env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
